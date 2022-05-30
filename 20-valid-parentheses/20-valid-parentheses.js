78f50cbc449a1910e1b6dd19c214df971fda6c06
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-05-29 19:26:15 -0700

    Time: 485 ms (5.03%), Space: 51.2 MB (5.14%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,30 +3,21 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    // const brackets = {
-    //     0 : '(',
-    //     1 : '{',
-    //     2 : '[',
-    //     3 : ')',
-    //     4 : '}',
-    //     5 : ']' 
-    // }
-    const brackets = {
-        '(' : ')',
-        '[' : ']',
-        '{' : '}'
-    }
-    const stack = []
-    
-    for (let i = 0; i < s.length; i++) {
-        if (brackets[s[i]]) {
-            stack.push(brackets[s[i]])
-        } else if (s[i] !== stack.pop()) {
-            return false
-        }
-        
-    }
-    
+   const brackets = {
+       '{' : '}',
+       '[' : ']',
+       '(' : ')'
+   }
+   const stack = []
+   
+   for (let i = 0; i< s.length; i++) {
+       console.log(stack)
+       if (brackets[s[i]] !== undefined) {
+           stack.push(brackets[s[i]])
+       } else {
+           if (stack.pop() !== s[i]) return false
+       }
+   }
     return stack.length === 0
 };
     
