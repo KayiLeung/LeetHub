b879fab0df9ec1849bd6ce35e532bb273c0ab6b9
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-05-31 19:23:36 -0700

    Time: 83 ms (37.75%), Space: 42 MB (47.24%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -5,15 +5,27 @@
 var climbStairs = function(n) {
     if (n <= 3 ) return n
     
-    let arr = []
-    arr[1] = 1
-    arr[2] = 2
+//     let arr = []
+//     arr[1] = 1
+//     arr[2] = 2
     
-    for (let i = 3; i <= n ; i++) {
-        arr[i] = arr[i - 1] + arr[i - 2]
+//     for (let i = 3; i <= n ; i++) {
+//         arr[i] = arr[i - 1] + arr[i - 2]
+//     }
+    
+//     return arr[n]
+    let i = 4
+    
+    let first = 2
+    let second = 3
+    
+    while (i <= n) {
+        temp = first
+        first =second
+        second = temp + first
+        i++
     }
     
-    return arr[n]
-    
+    return second
     
 };
