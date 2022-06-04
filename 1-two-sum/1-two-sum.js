2655f9bec2a6a370d4d64aff9695eda6d4af562a
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-06-04 16:14:05 -0700

    Time: 106 ms (57.58%), Space: 42.7 MB (44.65%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,17 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const prevNums = {}
     
-    for(let i = 0; i < nums.length; i++ ) {
-        const num = nums[i]
-        const subTarget = target - num
-        if (prevNums.hasOwnProperty(subTarget)) {
-            return [prevNums[subTarget], i]
+    let prevNums = {}
+    
+    for (let i = 0; i < nums.length; i++) {
+        let subTarget = target - nums[i]
+        if (prevNums[subTarget] === undefined) {
+            prevNums[nums[i]] = i
         } else {
-            prevNums[num] = i
+            return [prevNums[subTarget], i]
         }
     }
-    
     
 };
