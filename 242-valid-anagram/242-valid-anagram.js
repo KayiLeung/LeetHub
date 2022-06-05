e2c33722ae69dfb302e79d9f1468d54174be0e0f
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-06-04 18:05:24 -0700

    Time: 84 ms (87.54%), Space: 42.9 MB (84.25%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -7,21 +7,17 @@ var isAnagram = function(s, t) {
     if (s.length !== t.length) return false
     let charCount = {}
     
-    for(let i = 0; i < s.length; i++) {
-        if (charCount[s[i]] === undefined) {
-            charCount[s[i]] = 0
-        } 
+    for (let i = 0; i < s.length; i++) {
+        if (charCount[s[i]] === undefined) charCount[s[i]] = 0
         charCount[s[i]] += 1
     }
     for (let i = 0; i < t.length; i++) {
-        if (charCount[t[i]] === undefined ) {
-            return false
-        } else {
-            charCount[t[i]] -= 1
-        }
+        if (charCount[t[i]] === undefined) return false
+        
+        charCount[t[i]] -= 1
+        if (charCount[t[i]] < 0) return false
+        
     }
-    
-    const values = Object.values(charCount)
-    
-    return values.every(el => el === 0)
+
+    return true
 };
