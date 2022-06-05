5ee9588adba52dbb5ca1773109637ee8983b4c94
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-06-04 18:45:39 -0700

    Time: 133 ms (14.35%), Space: 44 MB (76.58%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -7,21 +7,24 @@
  */
 var floodFill = function(image, sr, sc, newColor) {
     const oldColor = image[sr][sc]
-    fill(image, sr, sc, oldColor, newColor)
+    
+    fillColor(image, sr, sc, oldColor, newColor)
+  
     
     return image
-};
+}
 
-var fill = (image, x, y, oldColor, newColor) => {
-    
-    if (x < 0 || y < 0 || x >= image.length || y >= image[x].length || image[x][y] === newColor || image[x][y] !== oldColor) {
-        return
+var fillColor = (image, x, y, oldColor, newColor) => {
+    if (x < 0 || x >= image.length ||
+        y < 0 || y >= image[0].length ||
+        image[x][y] === newColor ||
+        image[x][y] !== oldColor) {
+        return 
     }
     image[x][y] = newColor
     
-    fill (image, x + 1, y, oldColor, newColor)
-    fill (image, x - 1, y, oldColor, newColor)
-    fill (image, x, y + 1, oldColor, newColor)
-    fill (image, x, y - 1, oldColor, newColor)
-    
+    fillColor (image, x + 1, y, oldColor, newColor)
+    fillColor (image, x - 1, y, oldColor, newColor)
+    fillColor (image, x, y + 1, oldColor, newColor)
+    fillColor (image, x, y - 1, oldColor, newColor)
 }
