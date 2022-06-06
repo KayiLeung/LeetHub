bf0f9bdd5af4ede5f05925a21f5667db54abe03d
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-06-05 18:05:49 -0700

    Time: 123 ms (19.63%), Space: 44.6 MB (88.99%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -13,12 +13,13 @@
 var hasCycle = function(head) {
     let fast = head;
     while (fast && fast.next) {
+        
         head = head.next
         fast = fast.next.next;
-        
+      
         if (head === fast) return true
     }
-    
+
     return false
     
     
