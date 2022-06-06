4a85a14319b593d1a4bb9ae4e2e623623669feec
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-06-06 04:54:58 -0700

    Time: 67 ms (82.10%), Space: 42 MB (77.09%) - LeetHub

--- 50-powx-n/50-powx-n.js
+++ 50-powx-n/50-powx-n.js
@@ -6,18 +6,8 @@
 var myPow = function(x, n) {
     if (n === 0) return 1
     let pow = Math.abs(n)
+    let res
+    pow % 2 === 1 ? res = myPow(x*x, (pow - 1)/2)*x : res = myPow(x*x, pow/2)
     
-    let ans = 1.00
-    
-    while (pow > 0) {
-        if (pow % 2 === 1) {
-            ans = ans*x
-            x = x*x
-            pow = Math.floor(pow / 2)
-        } else {
-            x = x*x
-            pow = pow / 2
-        }
-    }
-    return n > 0 ? ans : 1/ans
+    return n > 0 ? res : 1/res
 }
