996bf6c2636ac13def158594a0e505515774afeb
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-06-18 18:20:54 -0700

    Time: 132 ms (66.30%), Space: 54.2 MB (64.90%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let answer = new Array(nums.length).fill(1);
+    let tallyUp = 1;
+    let tallyDown = 1;
+    
+    for (let i = 1, j = nums.length - 2; i < nums.length; i++, j--) {
+        tallyUp *= nums[i - 1];
+        tallyDown *= nums[j + 1];
+        answer[i] *= tallyUp;
+        answer[j] *= tallyDown;
+    }
+    
+    return answer
+};
