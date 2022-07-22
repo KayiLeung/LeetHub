33b9c3a87f41ebe70a15cac793dbb82a732860bd
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-22 11:28:39 -0700

    Time: 106 ms (22.16%), Space: 42.5 MB (20.58%) - LeetHub

--- 75-sort-colors/75-sort-colors.js
+++ 75-sort-colors/75-sort-colors.js
@@ -2,19 +2,27 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function(nums) {
-    let sorted = false
-    
-    while(!sorted) {
-        sorted = true
-        for (let i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                const temp = nums[i]
-                nums[i] = nums[i + 1]
-                nums[i + 1] = temp
-                sorted = false
-            }
-        }
+var sortColors = function (nums) {
+  let low = 0;
+  let high = nums.length - 1;
+  let i = low;
+
+  while (i <= high) {
+    switch (nums[i]) {
+      case 0:
+        [nums[i], nums[low]] = [nums[low], nums[i]]; // swap zero w/ the element at index:low
+        low++;
+        i++;
+        break;
+
+      case 1:
+        i++;
+        break;
+
+      case 2:
+        [nums[i], nums[high]] = [nums[high], nums[i]];
+		high--;
+        break;
     }
-    return nums
+  }
 };
