23fcc002be10686683e0588af5162757fc784ebc
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-23 14:14:15 -0700

    Time: 176 ms (88.37%), Space: 49 MB (97.32%) - LeetHub

--- 542-01-matrix/542-01-matrix.js
+++ 542-01-matrix/542-01-matrix.js
@@ -3,63 +3,55 @@
  * @return {number[][]}
  */
 var updateMatrix = function(mat) {
-//     const m = mat.length, n = mat[0].length;
+
+    // const r = mat.length
+    // const c = mat[0].length
+    // const isVaild = (x, y) => ( 0 <= x && x < r && 0 <= y && y < c)
+    // const dirs = [ [0,1], [0,-1], [1,0], [-1,0] ]
+    // const dis = new Array(r).fill(-1).map( () => new Array(c).fill(r * c))
+    // const queue = []
+    // for (let row = 0 ; row < r; row++) {
+    //     for(let col = 0; col < c; col++) {
+    //         if (mat[row][col] === 0) {
+    //             queue.push([row, col])
+    //             dis[row][col] = 0 
+    //         }
+    //     }
+    // }
+    // while (queue.length > 0) {
+    //     const [x, y] = queue.shift()
+    //     for (let dir of dirs) {
+    //         const [dirX, dirY] = dir
+    //         const X = dirX + x
+    //         const Y = dirY + y
+    //         if(isVaild(X, Y) && dis[x][y] + 1 < dis[X][Y]) {
+    //            queue.push([X, Y]);
+    //             dis[X][Y] = dis[x][y] + 1
+    //            }
+    //     }
+    // }
+    // return dis
     
-//     const isValid = (x, y) => !(x < 0 || y < 0 || x >= m || y >= n);
-//     const dir = [0, 1, 0, -1, 0];
-    
-//     const MAX = m * n;
-//     const dis = new Array(m).fill(-1).map(() => new Array(n).fill(MAX));
-//     const Q = [];
-    
-//     mat.forEach((row, i) => {
-//         row.forEach((el, j) => {
-//             if(mat[i][j] == 0) {
-//                 dis[i][j] = 0;
-//                 Q.push([i, j]);
-//             }
-//         })
-//     });
-    
-//     while(Q.length) {
-//         const [x, y] = Q.shift();
-//         const d = dis[x][y];
-//         for(let i = 0; i < 4; i++) {
-//             const X = x + dir[i], Y = y + dir[i+1];
-//             if(isValid(X, Y) && d + 1 < dis[X][Y]) {
-//                 Q.push([X, Y]);
-//                 dis[X][Y] = d + 1;
-//             }
-//         }
-//     }
-    
-//     return dis;
     const r = mat.length
     const c = mat[0].length
-    const isVaild = (x, y) => ( 0 <= x && x < r && 0 <= y && y < c)
-    const dirs = [ [0,1], [0,-1], [1,0], [-1,0] ]
-    const dis = new Array(r).fill(-1).map( () => new Array(c).fill(r * c))
-    const queue = []
-    for (let row = 0 ; row < r; row++) {
-        for(let col = 0; col < c; col++) {
-            if (mat[row][col] === 0) {
-                queue.push([row, col])
-                dis[row][col] = 0 
-            }
+    
+    for (let row = 0; row < r; row++) {
+        for (let col = 0; col < c; col++) {
+            if (mat[row][col] === 0) continue;
+            const top = (row - 1 >= 0) ? mat[row - 1][col] : Infinity
+            const left = (col - 1 >= 0) ? mat[row][col -1] : Infinity
+            mat[row][col] = Math.min(top, left) + 1
         }
     }
-    while (queue.length > 0) {
-        const [x, y] = queue.shift()
-        for (let dir of dirs) {
-            const [dirX, dirY] = dir
-            const X = dirX + x
-            const Y = dirY + y
-            if(isVaild(X, Y) && dis[x][y] + 1 < dis[X][Y]) {
-               queue.push([X, Y]);
-                dis[X][Y] = dis[x][y] + 1
-               }
+    
+        for (let row = r - 1; row >= 0; row--) {
+        for (let col = c - 1; col >= 0; col--) {
+            if (mat[row][col] === 0) continue;
+            const bottom = (row + 1 < r) ? mat[row + 1][col] : Infinity
+            const right = (col + 1 < c) ? mat[row][col + 1] : Infinity
+            mat[row][col] = Math.min(mat[row][col], (Math.min(bottom, right) + 1))
         }
     }
-    return dis
+    return mat
 };
 
