032b091cbf964666876b1c9e011519ef63afb1fe
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-22 20:44:21 -0700

    Time: 83 ms (83.40%), Space: 44.7 MB (22.53%) - LeetHub

--- 57-insert-interval/57-insert-interval.js
+++ 57-insert-interval/57-insert-interval.js
@@ -4,27 +4,24 @@
  * @return {number[][]}
  */
 var insert = function (intervals, newInterval) {
-    let i = 0 
-    let insert 
-    while (i < intervals.length){
-        let [a,b] = intervals[i]
-        let [x,y] = newInterval
-
-        if ((a >= x && a <= y ) ||
-            (b >= x && b <= y) ||
-            (x >= a && x <= b) ||
-            (y >= a && y <= b)
-           ) {
+    let i = 0
+    while (i < intervals.length) {
+        const [a, b] = intervals[i]
+        const [x, y] = newInterval
+        if ( (a >= x && a <= y) || 
+             (b >= x && b <= y) ||
+             (x >= a && x <= b) ||
+             (y >= a && x <= b)) {
             newInterval = mergeInterval(intervals[i], newInterval)
-            intervals.splice(i, 1);
-             } else {
-                 i++
-             }
-        
+            intervals.splice(i , 1)
+        } else {
+            i++
+        }
     }
     intervals.push(newInterval)
-    return intervals.sort((a,b) => a[0] - b[0])
+    return intervals.sort((a, b) => a[0] - b[0])
 };
+
 
 const mergeInterval = (int1, int2) => [
     Math.min(int1[0], int2[0]), Math.max(int1[1], int2[1])
