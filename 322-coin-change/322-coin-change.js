454a9f61948270fc671ac1bbd0868381925cd0bd
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-23 23:39:50 -0700

    Time: 114 ms (91.89%), Space: 46 MB (76.61%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -5,16 +5,15 @@
  */
 var coinChange = function(coins, amount) {
     let dp = new Array(amount + 1).fill(Infinity)
+    
     dp[0] = 0
-    
     for (let i = 1; i <= amount; i++) {
-        let minCoin = Infinity
-        for (coin of coins) {
-            if (coin > i) continue;
-            minCoin = Math.min(minCoin, dp[i - coin])
+        let minChange = Infinity
+        for (let coin of coins) {
+            if (coin > i) continue
+           minChange = Math.min(minChange, dp[i - coin]) 
         }
-        dp[i] = minCoin + 1
+        dp[i] = minChange + 1
     }
-    
     return dp[amount] === Infinity ? -1 : dp[amount]
 };
