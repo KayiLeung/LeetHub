b13b2c19bb43b2384c5ea978daabced9ced71ef9
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-24 00:24:08 -0700

    Time: 108 ms (26.40%), Space: 44.1 MB (12.92%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.js
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.js
@@ -4,20 +4,24 @@
  * @return {number}
  */
 var convertTime = function(current, correct) {
-    let operations = [1, 5, 15, 60]
+    let operations = [60, 15, 5, 1]
     a = current.split(":")
     b = correct.split(":")
-    let diff = (parseInt((b[0])) * 60 + parseInt(b[1])) - (parseInt((a[0])) * 60 + parseInt(a[1]))
+    let diff = (b[1] - a[1]) + ((b[0] - a[0])*60)
+    console.log(diff)
     
-    let dp = new Array(diff + 1).fill(Infinity)      
-    dp[0] = 0
+    count = 0
     
-    for(let i = 1; i <= diff; i++) {
-        let count = Infinity
-        for(let o of operations) {
-            if(i - o < 0) continue
-            dp[i] = Math.min(dp[i] , dp[i - o] + 1)
+    for (let o of operations) {
+        while (diff >= o) {
+                diff -= o
+                count++
         }
     }
-    return dp[diff]
+    return count
 };
+
+//         while(minDifference >= difference){
+//             minDifference -= difference;
+//             operations++;
+//         }
