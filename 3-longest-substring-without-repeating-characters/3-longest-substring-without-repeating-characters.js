4688f39d8a13440d7d444161533fba409b20e45c
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-23 17:01:36 -0700

    Time: 77 ms (98.92%), Space: 45 MB (90.85%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,17 +3,16 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    var start = 0, maxLen = 0
-    let map = new Map();
+    let start = 0
+    let maxLen = 0
+    let map = new Map()
     
-    for (let i = 0; i < s.length; i ++) {
-        var char = s[i]
-        
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i]
         if(map.get(char) >= start) start = map.get(char) + 1;
-        
         map.set(char, i)
         
-        maxLen = Math.max(maxLen, (i - start + 1))
+        maxLen = Math.max(maxLen, ( i - start + 1))
     }
     
     return maxLen
