def40c39e749e0e360d4f7fbf4453cd2edf8acd8
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-24 14:30:33 -0700

    Time: 89 ms (78.55%), Space: 45.6 MB (16.57%) - LeetHub

--- 46-permutations/46-permutations.js
+++ 46-permutations/46-permutations.js
@@ -16,9 +16,23 @@ var permute = function(nums) {
         for(let j=0; j<nextPermutations.length; j++){
             nextPermutations[j].unshift(num);
         }
-        console.log(nextPermutations)
         permutations.push(...nextPermutations);
     }
     
     return permutations;
+//     if (nums.length === 0) return [[]]
+    
+//     const firstEl = nums[0]
+//     const rest = nums.slice(1)
+    
+//     const permsWithoutFirst = permute(rest)
+//     const allPermutations = []
+    
+//     for (let perm of permsWithoutFirst) {
+//         for (let i = 0; i < perm.length; i++) {
+//             const permWithFirst = [...perm.slice(0, i), firstEl, ...perm.slice(i)]
+//             allPermutations.push(permWithFirst)
+//         }
+//     }
+//     return allPermutations
 };
