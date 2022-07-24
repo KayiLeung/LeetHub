411f684ea0047db69ead5e810b1fe3f92a6db322
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-23 18:38:09 -0700

    Time: 96 ms (50.65%), Space: 43.7 MB (94.97%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
@@ -13,19 +13,31 @@
 var levelOrder = function(root) {
   if (!root) return []
   let res = []
-  let queue = [{node: root, levelNum: 0}]
+//   let queue = [{node: root, levelNum: 0}]
   
+//   while (queue.length > 0) {
+//     const {node, levelNum} = queue.shift();
+    
+//     if (res.length === levelNum) {
+//       res[levelNum] = [node.val]
+//     } else {
+//       res[levelNum].push(node.val)
+//     }
+    
+//     if (node.left) queue.push({node: node.left, levelNum: levelNum + 1})
+//     if (node.right) queue.push({node: node.right, levelNum: levelNum + 1})
+//   }
+  
+  let queue = [[root, 0]]
   while (queue.length > 0) {
-    const {node, levelNum} = queue.shift();
-    
-    if (res.length === levelNum) {
-      res[levelNum] = [node.val]
-    } else {
-      res[levelNum].push(node.val)
-    }
-    
-    if (node.left) queue.push({node: node.left, levelNum: levelNum + 1})
-    if (node.right) queue.push({node: node.right, levelNum: levelNum + 1})
+      const [node, lvl] = queue.shift()
+      if (res.length === lvl) {
+          res[lvl] = [node.val]
+      } else {
+          res[lvl].push(node.val)
+      }
+      if (node.left) queue.push([node.left, lvl + 1])
+      if (node.right) queue.push([node.right, lvl + 1])
   }
   return res
 };
