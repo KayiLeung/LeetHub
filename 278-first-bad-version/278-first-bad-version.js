9fb8fca9af66d1d76af62f6009f0460467ae0fdd
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-26 15:37:18 -0700

    Time: 68 ms (81.80%), Space: 42.1 MB (29.41%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let low = 1
+        let end = n
+        let mid
+        
+        while (low <= end) {
+            mid = Math.floor((low + end) /2)
+            if (isBadVersion(mid)) {
+                if (!isBadVersion(mid - 1)) return mid;
+                end = mid - 1
+            } else {
+                if (isBadVersion(mid + 1)) return mid + 1
+                low = mid + 1
+            }
+        }
+    }
+    
+    
+    
+};
