8e5ce3e92110663c4ebfca9825343c933da7a31a
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-26 11:49:19 -0700

    Time: 112 ms (47.13%), Space: 44.4 MB (73.87%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -5,14 +5,15 @@
 var isPalindrome = function(s) {
     
     let newStr = s.replace(/[^a-z0-9]/gi, '').toLowerCase()
+    
     let left = 0
     let right = newStr.length - 1
     
-    while (left < right) {
+    while (left <= right) {
         if (newStr[left] !== newStr[right]) return false
+    
         left++
         right--
     }
-    
     return true
 };
