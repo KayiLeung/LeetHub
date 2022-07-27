94bdcde53df322633cab5bcfed2294666c0cacbc
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-27 16:46:28 -0700

    Time: 212 ms (23.61%), Space: 50.8 MB (63.55%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,6 +3,17 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-   let arrSet = new Set(nums)
-   return arrSet.size !== nums.length
+   nums = nums.sort((a, b) =>  a- b)
+    
+    let i = 0
+    let j = nums.length - 1
+    
+    while (i <= j) {
+        if (nums[i] === nums[ i + 1]) return true
+        if (nums[j] === nums[j - 1]) return true
+        
+        i++
+        j--
+    }
+    return false
 };
