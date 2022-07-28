25b35fc8deca7bc64bbdcf0cd2a285767b7b7e23
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-27 23:56:16 -0700

    Time: 135 ms (29.16%), Space: 45.3 MB (38.38%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.js
+++ 994-rotting-oranges/994-rotting-oranges.js
@@ -3,24 +3,23 @@
  * @return {number}
  */
 var orangesRotting = function(grid) {
-    const m = grid.length;
-    const n = grid[0].length;
+    const m = grid.length
+    const n = grid[0].length
+    let minutes = 0
     
-    const queue = [];
-    let fresh = 0;
-    let minutes = 0;
-    
+    let fresh = 0
+    let queue = []
     for (let r = 0; r < m; r++) {
-        for (let c = 0; c < n; c++) {
+        for(let c = 0; c < n; c++) {
             if (grid[r][c] === 1) {
-                fresh++;
-            } else if (grid[r][c] === 2) {
-                queue.push([r, c]);
+                fresh++
+            } else if (grid[r][c] === 2){
+                queue.push([r, c])
             }
         }
     }
     
-    const directions = [[-1, 0], [0, -1], [1, 0], [0, 1]];
+    const dirs = [[1,0],[-1,0],[0,1],[0,-1]]
     
     while (queue.length && fresh) {
         minutes++;
@@ -29,7 +28,7 @@ var orangesRotting = function(grid) {
         while (curLen--) {
             const [qr, qc] = queue.shift();
             
-            for (const [dr, dc] of directions) {
+            for (const [dr, dc] of dirs) {
                 const r = qr + dr;
                 const c = qc + dc;
                 
@@ -44,7 +43,8 @@ var orangesRotting = function(grid) {
         }
     }
     
-    return fresh ? -1 : minutes;
+    
+    return fresh === 0 ? minutes : -1
 };
 
 
