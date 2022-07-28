2a6d8d229a60a49c16db152b803c8abfbfc1a986
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-28 01:03:52 -0700

    Time: 83 ms (56.81%), Space: 42.2 MB (58.04%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -4,9 +4,17 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] >= target) return i;
+    let start = 0
+    let end = nums.length - 1
+    while (start <= end) {
+        let mid = Math.floor((start + end) /2)
+        if (target > nums[mid]) {
+            start = mid + 1
+        } else if (target < nums[mid]) {
+            end = mid - 1
+        } else {
+            return mid
+        }
     }
-    
-    return nums.length
+    return start
 };
