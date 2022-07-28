667623507e4022ea5a3fb01c75c98ca974cda44f
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-28 00:35:39 -0700

    Time: 66 ms (95.04%), Space: 44.6 MB (95.80%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -5,17 +5,17 @@
  */
 var search = function(nums, target, res = 0) {
     let start = 0
-    let end = nums.length - 1
-    let med
+    let end = nums.length -1
+    let mid
     
     while (start <= end) {
-    med = Math.floor((start + end)/2)
-        if (nums[med] === target) {
-            return med
-        } else if (nums[med] < target){
-            start = med + 1
+        mid = Math.floor((start + end) /2)
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] > target) {
+            end = mid - 1
         } else {
-            end = med - 1
+            start = mid + 1
         }
     }
     return -1
