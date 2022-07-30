f7f9730c45bb0e61b9c78f987e1fd041203af320
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-07-30 01:22:34 -0700

    Time: 142 ms (57.21%), Space: 50.3 MB (5.59%) - LeetHub

---
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function(nums) {
+    nums = nums.sort((a, b) => a - b)
+    
+    let res = []
+    for (let i = 0; i < nums.length - 2; i++) {
+        const first = nums[i]
+        const second = nums[i + 1]
+        const third = nums[i + 2]
+        if((first + second > third) &&
+           (second + third > first) &&
+           (first + third > second)) {
+            res.push(first + second + third)
+        }
+    }
+    console.log(res)
+    return res.length === 0 ? 0 : Math.max(...res)
+};
