6d41f1564b6781c883f6e2d612cf9021686ef56f
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-08-01 22:09:15 -0700

    Time: 236 ms (33.17%), Space: 49.6 MB (27.05%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -3,22 +3,42 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function(coins, amount) {
-    let dp = new Array(amount + 1).fill(Infinity)
+// var coinChange = function(coins, amount) {
+//     let dp = new Array(amount + 1).fill(Infinity)
     
-    dp[0] = 0
+//     dp[0] = 0
     
     
-    for (let i = 1; i <= amount; i++) {
-//         i is the curr amount, so if coin is larger than i, means that it could be to give change
-        let minChange = Infinity
-        for (let coin of coins) {
+//     for (let i = 1; i <= amount; i++) {
+// //         i is the curr amount, so if coin is larger than i, means that it could be to give change
+//         let minChange = Infinity
+//         for (let coin of coins) {
             
-            if (coin > i) continue;
-            minChange = Math.min(minChange, dp[i - coin])
-        }
-        dp[i] = minChange + 1
-    }
+//             if (coin > i) continue;
+//             minChange = Math.min(minChange, dp[i - coin])
+//         }
+//         dp[i] = minChange + 1
+//     }
     
-    return dp[amount] === Infinity ? -1 : dp[amount]
+//     return dp[amount] === Infinity ? -1 : dp[amount]
+// };
+
+var coinChange = function(coins, amount) {
+  const answer = _minChange(amount, coins);
+  return answer === Infinity ? -1 : answer;
+};
+
+const _minChange = (amount, coins, memo = {}) => {
+  if (amount < 0) return Infinity;
+  
+  if (amount === 0) return 0;
+  
+  if (amount in memo) return memo[amount];
+  
+  let min = Infinity;
+  for (let coin of coins) {
+    const numCoins = 1 + _minChange(amount - coin, coins, memo);
+    min = Math.min(numCoins, min);
+  }
+  return memo[amount] = min;
 };
