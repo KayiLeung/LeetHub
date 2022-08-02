ff5d7cf219ac87bac543f4798d0e96b56423874a
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-08-01 21:41:01 -0700

    Time: 188 ms (49.61%), Space: 45.4 MB (97.40%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -7,13 +7,18 @@ var coinChange = function(coins, amount) {
     let dp = new Array(amount + 1).fill(Infinity)
     
     dp[0] = 0
+    
+    
     for (let i = 1; i <= amount; i++) {
+//         i is the curr amount, so if coin is larger than i, means that it could be to give change
         let minChange = Infinity
         for (let coin of coins) {
-            if (coin > i) continue
-           minChange = Math.min(minChange, dp[i - coin]) 
+            
+            if (coin > i) continue;
+            minChange = Math.min(minChange, dp[i - coin])
         }
         dp[i] = minChange + 1
     }
+    
     return dp[amount] === Infinity ? -1 : dp[amount]
 };
