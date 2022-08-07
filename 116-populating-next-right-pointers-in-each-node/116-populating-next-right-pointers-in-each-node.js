17df644fb239933f5908f55d5ffd98e0dc3fb9e8
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-08-06 19:21:03 -0700

    Time: 110 ms (64.36%), Space: 48.4 MB (81.32%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,29 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    let [curr, next] = [root, root ? root.left : null];
+    
+    while(curr && next) {
+        curr.left.next = curr.right
+        if(curr.next) curr.right.next = curr.next.left
+        
+        curr = curr.next
+        if(!curr) {
+            curr = next
+            next = curr.left
+        }
+    }
+    return root
+};
