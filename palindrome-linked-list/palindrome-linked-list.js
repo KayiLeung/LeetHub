20df75063119324160b49c92414dbc65b229e998
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-08-16 22:22:39 -0700

    Time: 198 ms, Memory: 77.9 MB - LeetHub

---
+++ palindrome-linked-list/palindrome-linked-list.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let arr = []
+    let curr = head
+    while (curr) {
+        arr.push(curr.val)
+        curr = curr.next
+    }
+    
+    let i = 0
+    let j = arr.length - 1
+    while (i < j) {
+        if (arr[i] !== arr[j]) return false
+        i++
+        j--
+    }
+    
+    return true
+};
