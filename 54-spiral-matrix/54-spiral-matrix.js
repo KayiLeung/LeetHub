3e96091792360c10a9002ba0bfeebb1455b4e301
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-08-19 00:11:18 -0700

    Time: 104 ms (18.10%), Space: 41.9 MB (48.16%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.js
@@ -0,0 +1,57 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    let row = matrix.length
+    let col = matrix[0].length
+    
+    let dir  = [[0,1], [1,0], [0,-1],[-1,0]]
+    
+    let res = []
+    let size = row * col
+    let x = 0
+    let y = 0
+    let X = 0
+    let Y = 0
+    let eX = row
+    let eY = col
+    while (res.length < size) {
+        
+        while(y < eY) {
+            if (res.length == size)  return res
+            res.push(matrix[x][y])
+            y++
+        }
+        y -= 1
+        x += 1
+        while(x < eX) {
+            if (res.length == size)  return res
+            res.push(matrix[x][y])
+            x++
+        }
+        x -= 1
+        y -= 1
+
+        while(y >= Y) {
+            if (res.length == size)  return res
+            res.push(matrix[x][y])
+            y--
+        }
+        y += 1
+        x -= 1
+        while(x >= X + 1) {
+            if (res.length == size)  return res
+            res.push(matrix[x][y])
+            x--
+        }
+        x += 1
+        y += 1
+        
+        X += 1
+        Y += 1
+        eX -= 1
+        eY -= 1
+    }
+        return res
+}
