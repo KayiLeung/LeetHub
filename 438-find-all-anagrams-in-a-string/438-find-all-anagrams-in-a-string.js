c354347d246f417f88e14804b84991b8b922cffc
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-08-31 17:16:35 -0700

    Time: 112 ms (92.46%), Space: 46.9 MB (62.36%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+	var findAnagrams = function (s, p) {
+
+		const arr = [];
+		const obj = {};
+
+		for (let i of p) {
+			obj[i] ? obj[i] += 1 : obj[i] = 1;
+		}
+
+		let left = 0;
+		let right = 0;
+		let count = p.length;
+
+		while (right < s.length) {
+			if (obj[s[right]] > 0) count--;
+
+			obj[s[right]]--;
+			right++;
+
+			if (count === 0) arr.push(left);
+
+			if (right - left == p.length) {
+
+				if (obj[s[left]] >= 0) count++;
+
+				obj[s[left]]++;
+				left++;
+			}
+		}
+		return arr;
+	};
