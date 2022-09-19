698beebe58ed415214a2c346284c2f6eb797a3ff
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-09-18 18:14:47 -0700

    Time: 183 ms (36.50%), Space: 54.7 MB (51.70%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,16 +3,14 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let answer = new Array(nums.length).fill(1)
+    let res = new Array(nums.length).fill(1)
     let tallyUp = 1
     let tallyDown = 1
-    
-    for (let i = 1, j = nums.length - 2; i < nums.length; i++, j--) {
-        tallyUp *= nums[i - 1];
-        tallyDown *= nums[j + 1];
-        answer[i] *= tallyUp;
-        answer[j] *= tallyDown;
+    for(let i = 1, j = nums.length -2; i < nums.length; i++, j--) {
+        tallyUp *= nums[i - 1]
+        tallyDown *= nums[j + 1]
+        res[i] *= tallyUp
+        res[j] *= tallyDown
     }
-    return answer
-    
+    return res
 };
