358bfe487a737dbdd0d434ffd22309ea9f9ce5b9
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-09-26 16:32:15 -0700

    Time: 380 ms (32.15%), Space: 72.9 MB (29.70%) - LeetHub

---
+++ 310-minimum-height-trees/310-minimum-height-trees.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+var findMinHeightTrees = function(n, edges) {
+    if (n <= 0) return []
+    if (n === 1) return [0]
+    
+    let graph = {}
+    let indegree = Array(n).fill(0)
+    for (const [a ,b] of edges) {
+        if (!(a in graph)) graph[a] = []
+        if (!(b in graph)) graph[b] = []
+        graph[a].push(b)
+        graph[b].push(a)
+        indegree[a]++
+        indegree[b]++
+    }
+    
+    let leaves = []
+    for (let i = 0 ; i < n; i++) {
+        if(indegree[i] === 1) leaves.push(i)
+    }
+    
+    let total = n
+    
+    while (total > 2) {
+        let size = leaves.length
+        total -= size
+        
+        for(let i = 0; i < size; i++) {
+            let vertex = leaves.shift()
+            for(let edge of graph[vertex]) {
+                indegree[edge]--
+                if(indegree[edge] === 1) leaves.push(edge)
+            }
+        }
+    }
+    return leaves
+};
+
+
+
+
+
+
