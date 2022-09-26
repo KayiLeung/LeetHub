afe800da7a6e9c003251f0d0e38b97ac38d38625
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-09-26 16:02:24 -0700

    Time: 145 ms (78.16%), Space: 46.1 MB (71.53%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
@@ -5,32 +5,57 @@
  */
 	var findAnagrams = function (s, p) {
 
-		const arr = [];
-		const obj = {};
+// 		const arr = [];
+// 		const obj = {};
 
-		for (let i of p) {
-			obj[i] ? obj[i] += 1 : obj[i] = 1;
-		}
+// 		for (let i of p) {
+// 			obj[i] ? obj[i] += 1 : obj[i] = 1;
+// 		}
 
-		let left = 0;
-		let right = 0;
-		let count = p.length;
+// 		let left = 0;
+// 		let right = 0;
+// 		let count = p.length;
 
-		while (right < s.length) {
-			if (obj[s[right]] > 0) count--;
+// 		while (right < s.length) {
+// 			if (obj[s[right]] > 0) count--;
 
-			obj[s[right]]--;
-			right++;
+// 			obj[s[right]]--;
+// 			right++;
 
-			if (count === 0) arr.push(left);
+// 			if (count === 0) arr.push(left);
 
-			if (right - left == p.length) {
+// 			if (right - left == p.length) {
 
-				if (obj[s[left]] >= 0) count++;
+// 				if (obj[s[left]] >= 0) count++;
 
-				obj[s[left]]++;
-				left++;
-			}
-		}
-		return arr;
+// 				obj[s[left]]++;
+// 				left++;
+// 			}
+// 		}
+// 		return arr;
+        const res = []
+        const chars = {}
+        for (let char of p) {
+            if(!(char in chars)) chars[char] = 0
+            chars[char] += 1
+        }
+        let left = 0
+        let right = 0
+        let count = p.length
+        
+        while (right < s.length) {
+            const char = s[right]
+            if (chars[char] > 0) count--
+            chars[char]--
+            right++
+            if (count === 0) res.push(left)
+            if (right - left === p.length) {
+                const ch = s[left]
+                if (chars[ch] >= 0) count++
+                chars[ch]++
+                left++
+            }
+        }
+        return res
+        
 	};
