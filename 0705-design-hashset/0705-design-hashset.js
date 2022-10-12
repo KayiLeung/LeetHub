2d64ead8143a962a5ec31656bf18ad3e6f7731e2
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-10-12 11:24:21 -0700

    Time: 873 ms (7.42%), Space: 81.1 MB (5.00%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.js
@@ -0,0 +1,53 @@
+
+var MyHashSet = function() {
+    this.hash = []
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function(key) {
+    for (let ele of this.hash) {
+        if (ele === key) {
+            return this.hash
+        }
+    }
+    return this.hash.push(key)
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function(key) {
+    let prev = []
+    for (let ele of this.hash) {
+        if (ele !== key) {
+            prev.push(ele)
+        }
+    }
+    this.hash = prev
+    
+};
+
+/** 
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function(key) {
+    for (let ele of this.hash) {
+        if (ele === key) {
+           return true
+        }
+    }
+    return false
+};
+
+/** 
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
