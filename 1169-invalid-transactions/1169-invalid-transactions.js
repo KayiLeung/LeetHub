fdc961ea3960a8b3b77205405c333403a3b4fa09
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-10-14 20:52:26 -0700

    Time: 171 ms (61.16%), Space: 47.5 MB (80.80%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.js
+++ 1169-invalid-transactions/1169-invalid-transactions.js
@@ -3,37 +3,37 @@
  * @return {string[]}
  */
 var invalidTransactions = function(transactions) {
-    const nameList = [];
-    const timeList = [];
-    const amountList = [];
-    const locationList = [];
-    const map = new Map();
-    for(let i = 0; i < transactions.length; i++){
-        map.set(i, false);
+    let nameList = [], timeList = [], amountList = [], cityList = []
+    let invalid = new Map()
+    let res = []
+    let len = transactions.length
+    
+    for (let i = 0 ; i < len; i++) {
+        invalid.set(i, false)
     }
-    const ans = [];
-    transactions.forEach(element => {
-        const t = element.split(',');
-        nameList.push(t[0]);
-        timeList.push(Number(t[1]));
-        amountList.push(Number(t[2]));
-        locationList.push(t[3]);
-    });
-    for(let i = 0; i < transactions.length; i++) {
-        if(amountList[i] > 1000) {
-            map.set(i, true);
+    
+    for (let tran of transactions) {
+        const [name, time, amount, city] = tran.split(',')
+        nameList.push(name)
+        timeList.push(Number(time))
+        amountList.push(Number(amount))
+        cityList.push(city)
+    }
+    
+    for (let i = 0; i < len; i++) {
+        if (amountList[i] > 1000) {
+            invalid.set(i, true)
         }
-        for(let j = i + 1; j < transactions.length; j++) {
-            if(nameList[i] === nameList[j] && Math.abs(timeList[i] - timeList[j]) <= 60 && locationList[i] !==                   locationList[j]){
-                map.set(i, true);
-                map.set(j, true);
+        for (let j = i + 1; j < len; j++) {
+            if (nameList[i] === nameList[j] && Math.abs(timeList[i] - timeList[j]) <= 60 && cityList[i] !== cityList[j]) {
+                invalid.set(i, true)
+                invalid.set(j, true)
             }
         }
     }
-    map.forEach((value, index) => {
-        if(value) {
-            ans.push(transactions[index]);
-        }
-    })
-    return ans;
+    
+    for (let i = 0 ; i < len; i++) {
+        if (invalid.get(i)) res.push(transactions[i])
+    }
+    return res
 };
