b5d16e177b12a3ca6603b4a941567a84d5746187
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-10-16 16:28:47 -0700

    Time: 152 ms (37.11%), Space: 50.2 MB (57.65%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -4,30 +4,37 @@
  * @return {string}
  */
 var removeDuplicates = function(s, k) {
-    
-    let stack = [] // [char, count]
+    //hashtable stores as array => [char, count]
+    let hash = []
     
     for (let char of s) {
-        if (stack.length > 0 && stack[stack.length - 1][0] === char) {
-            stack[stack.length - 1][1] += 1
+        if (!hash.length) {
+            hash.push([char, 1])
         } else {
-            stack.push([char, 1])
-        }
-        if (stack[stack.length - 1][1] === k) {
-            stack.pop()
+            if (hash[hash.length - 1][0] === char) {
+                hash[hash.length - 1][1] += 1
+                if (hash[hash.length - 1][1] === k) hash.pop()
+            } else {
+                hash.push([char, 1])
+            }
         }
     }
     let res = ''
-    for (let [char, count] of stack) {
+    for (let [char, count] of hash) {
         res += repeat(char, count)
     }
     return res
+    //if curr char is not same as last hash ele, push [curr char, count]
+    //if curr char same as last hash ele, last hash [curr char, count + 1]
+    //if curr char count === k , pop this out from hashtable
+    //return hashtable and use method join('') when iterate thur s
+    //time complexity O(n) n = s.length, space complexity worest case:O(n)
 };
 
-const repeat = (char, num) => {
-    let str = ''
-    for (let i = 0 ; i < num; i++) {
-        str += char
+const repeat = (char, count) => {
+    let res = ''
+    for (let i = 0 ; i < count; i++) {
+        res += char
     }
-    return str
+    return res
 }
