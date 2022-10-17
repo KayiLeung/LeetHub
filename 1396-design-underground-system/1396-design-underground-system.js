4a0533589fd4cd4f60f716eea3bc9d41db7237d8
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-10-16 22:03:17 -0700

    Time: 420 ms (36.84%), Space: 59.1 MB (74.61%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.js
+++ 1396-design-underground-system/1396-design-underground-system.js
@@ -1,8 +1,12 @@
-//checkIn and checkOut don;t return anything
-//average time return total sum / trips
+//use two hash table, one for checkIn, one for storing start and end station, and time for a trip
+//if customer checkin, use id as key and store it stationname and starting time
+//before checkout, fetch customers data by he/she's id
+//look for key in trips, if not, create a new key for this trip and storage total time of trip and count of trip
+//customer checkout, delete customer in hash table by userid
+//getAverageTime by look for start and end stations asa key, return total/count
 var UndergroundSystem = function() {
-    this.checkInCustomer = {} // this.checkInCustomer[id] = [startStation, startTime]
-    this.trips = {} //. this.trips[key] = [total sum, count]
+    this.checkInCustomer = {}  // this.checkInCustomer[id] = [stationName, t]
+    this.trips = {}  //this.trips[startStation+endStation] = [totalTime, count]
 };
 
 /** 
@@ -22,11 +26,12 @@ UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
  * @return {void}
  */
 UndergroundSystem.prototype.checkOut = function(id, stationName, t) {
-    const [startStation, startTime] = this.checkInCustomer[id]
+    const [startStation, startingTime] = this.checkInCustomer[id]
     const key = startStation + ',' + stationName
-    if (!(key in this.trips)) this.trips[key] = [0 ,0]
-    this.trips[key][0] += (t - startTime)
+    if (!(key in this.trips)) this.trips[key] = [0 , 0]
+    this.trips[key][0] += (t - startingTime)
     this.trips[key][1] += 1
+    delete this.checkInCustomer[id]
 };
 
 /** 
