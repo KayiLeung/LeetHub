391d78cf788311b48d9c7ab789ed337e86b9dd7e
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-10-16 18:53:18 -0700

    Time: 151 ms (20.71%), Space: 44.4 MB (69.31%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
@@ -3,25 +3,30 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-    const len = intervals.length
-    if (len < 2) return len
+    // looking for how many overlapped meetings 
+    //0.    5.   15.  
+    //10.   20.   30. 
+    // how many rooms need by looking for how the first meeting endtime
+    let startTime = []
+    let endTime = []
     
-    const start = [], end = []
-    
-    for (let i = 0 ; i < len; i++) {
-        const [s , e] = intervals[i]
-        start.push(s)
-        end.push(e)
+    for (let [start, end] of intervals) {
+        startTime.push(start) // [0, 5, 15]
+        endTime.push(end) // [30, 10, 20]
     }
-    start.sort((a , b) => a - b)
-    end.sort((a, b) => a - b)
     
-    let count = 0
-    let endPointer = 0
+    startTime.sort((a , b) => a - b) //[0,5, 15] => n log n
+    endTime.sort((a, b) => a - b) //[10, 20, 30] => n log n
+    let endIndex = 0
+    let rooms = 0
     
-    for (const s of start) {
-        if (end[endPointer] > s) count++
-        else endPointer++
+    for (let s of startTime) {              //15
+        if (s < endTime[endIndex]) {    // 15 < 10  => free room released
+            rooms++                     //2
+        } else {
+            endIndex++
+        }
     }
-    return count
+    
+    return rooms
 };
