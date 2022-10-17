644041899925dd66329a996e54aba27f42d546d4
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-10-16 23:51:22 -0700

    Time: 296 ms (38.57%), Space: 46.6 MB (88.79%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.js
+++ 1169-invalid-transactions/1169-invalid-transactions.js
@@ -3,32 +3,38 @@
  * @return {string[]}
  */
 var invalidTransactions = function(transactions) {
-    const nameList = [], timeList = [], amountList = [], cityList = []
-    let invalid = new Map() // [i , false]
-    for (let i = 0; i < transactions.length; i++) {
-        const [name, time, amount, city] = transactions[i].split(',')
-        nameList.push(name)
-        timeList.push(Number(time))
-        amountList.push(Number(amount))
-        cityList.push(city)
-        invalid.set(i , false)
-    }
+    let nameList = [], timeList = [], amountList = [], cityList = []
+    let invalid = new Map()
+    const len = transactions.length
     
-    for (let i = 0; i < transactions.length; i++) {
+    for (let i = 0; i < len; i++) {
+        invalid.set(i, false)
+    };
+    
+    for (let t of transactions) {
+        const [name, time, amount, city] = t.split(',')
+        nameList.push(name);
+        timeList.push(time);
+        amountList.push(amount);
+        cityList.push(city);
+    };
+    
+    for (let i = 0; i < len; i++) {
         if (amountList[i] > 1000) {
-            invalid.set(i, true)
-        }
-        for (let j = i + 1; j < transactions.length; j++) {
+            invalid.set(i, true);
+        };
+        for(let j = i + 1; j < len; j++) {
             const timeDiff = Math.abs(timeList[i] - timeList[j])
-            if ((nameList[i] === nameList[j]) && (timeDiff <= 60) && (cityList[i] !== cityList[j])) {
-                invalid.set(i, true)
-                invalid.set(j, true)
-            }
-        }
-    }
+            if ((nameList[i] === nameList[j]) && (cityList[i] !== cityList[j]) && (timeDiff <= 60)) {
+                invalid.set(i, true);
+                invalid.set(j, true);
+            };
+        };
+    };
+    
     let res = []
-    for (let i = 0 ; i < transactions.length; i++) {
-        if (invalid.get(i)) res.push(transactions[i])
+    for (let i = 0; i < len; i++) {
+        if(invalid.get(i)) res.push(transactions[i])
     }
     return res
 };
