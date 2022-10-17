4d578054ab26cd54ee41cc3e591982e1eba019cb
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-10-16 19:27:46 -0700

    Time: 88 ms (70.01%), Space: 43.9 MB (17.74%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.js
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.js
@@ -13,37 +13,39 @@
  * @return {Node}
  */
 var flatten = function(head) {
-    //dfs
-    //assign stack as array
-    //assign variable tail as pointer
-    //literate doubly link list from head to curr
-    //look for node with child, then change next pointer to child node
-    //disconnect next node prev pointer and push it in stack
-    //while node next and prev all null return and look the rest of node in stack
-    //pop out stack and repeat 
-    //return head while tail next and tail prev && stack is emtpy
-    
+    //dfs appoarch
+    //consider child node as left branch and next node as right branch
+    //iterate through the whole doubly list
+    //assign two variable, tail and curr, when tail shown the end of the passed node, and curr is the current node, and they are start at head
+    //if curr.next does exist, passed node become tail and curr become curr.next
+    //if curr.child does exist, push the curr.next into the stack, set curr.next.prev as null
+    //curr.child node become curr.next, reset curr.next and child.prev
+    //if curr.next and curr.child not exist, pop out node from stack as curr node and look for curr.next
+    //return head while stack is empty
     let stack = []
-    let curr = head
-    let tail = head
+    let curr = head, tail = head
     
     while (curr) {
         if (curr.child) {
-            const child = curr.child
             if (curr.next) {
                 const next = curr.next
                 next.prev = null
-                stack.push(next)
+                stack.push(next)   
             }
-            curr.next = child
+            
+            
+            const child = curr.child
             child.prev = curr
+            curr.next = child
             curr.child = null
         }
         tail = curr
         curr = curr.next
-    }
+    }   
+    //tail -> 12 -> 9 -> 10 -> 4 ...
+    //curr -> null -> 9 -> 10 -> null -> 4
     while (stack.length) {
-        curr = stack.pop()
+        curr = stack.pop()  // 4
         tail.next = curr
         curr.prev = tail
         while (curr) {
@@ -53,4 +55,5 @@ var flatten = function(head) {
     }
     
     return head
+    
 };
