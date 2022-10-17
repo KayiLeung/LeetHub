105c830b75471bcb217622d4e1439b5f1fc391ee
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-10-16 17:16:07 -0700

    Time: 95 ms (46.47%), Space: 42 MB (55.95%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -3,32 +3,40 @@
  * @return {string}
  */
 var decodeString = function(s) {
+    // stack,  if not ']', push curr char to stack
+    // if ']', look pop ele from stack until ele === '['
+    // pop out '['
+    //look for number
+    // use helper method, pass in popped char andd number and return repeated char
+    //push repeated char back to stack
+    // return stack.join('')  after iterate the whole s
+    // 3[a]2[bc]
     let stack = []
-    let nums = '0123456789'
-    for (let i = 0 ; i < s.length; i++) {
-        if (s[i] !== ']') {
-            stack.push(s[i])
+    const nums = '0123456789'
+    for (let char of s) {    //stack = [ ['3'] , ['aaa'], 2]
+        if (char !== ']') {      
+            stack.push(char)
         } else {
             let popped = ''
-            while (stack[stack.length - 1] !== '[') {
-                popped = stack.pop() + popped
+            while (stack[stack.length - 1] !== '[') { 
+                popped = stack.pop() + popped // a
             }
-            stack.pop()
-            let num = ''
-            while (nums.includes(stack[stack.length - 1])) {
-                num = stack.pop() + num
+            stack.pop() // pop out '['
+            let number = ''
+            while (nums.includes(stack[stack.length -  1])) {
+                number = stack.pop() + number
             }
-            stack.push(repeat(Number(num), popped))
+            const repeated = repeat(Number(number), popped) // (3 , 'a')  => 'aaa'
+            stack.push(repeated) 
         }
     }
-    
     return stack.join('')
 };
 
-const repeat = (num , str) => {
+const repeat = (num, str) => { //3, a
     let res = ''
-    for (let i = 0 ; i < num; i++) {
+    for (let i = 0; i < num; i++) {
         res += str
     }
-    return res
+    return res // 'aaa'
 }
