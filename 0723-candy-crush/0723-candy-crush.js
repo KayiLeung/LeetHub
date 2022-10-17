b81f86a4ff283935a9ecf868828211371bc1cde3
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-10-16 21:47:03 -0700

    Time: 136 ms (66.67%), Space: 45.1 MB (67.28%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.js
+++ 0723-candy-crush/0723-candy-crush.js
@@ -3,55 +3,58 @@
  * @return {number[][]}
  */
 var candyCrush = function(board) {
-    //check rows, if more than 3 or more candies => reassign value to negative vale
-    //check col, same as rows
-    //set pointer for each row, if that value is larger than zero, which move pointer--
-    //if value is lower than zero, ressign current to pointer row
-    if (!board) return board
+    //
+    //looks for row => 3 or more candies besided in row, and reassign those values as -values
+    //looks for col => 3 or more candies besided in col
+    //if value === 0, pass it
+    //drop positive value to the bottom boundary
+    //look for second round, recursively 
+    //return board until there are more same adjacent values
+    
     let isDone = true
     const col = board[0].length
     const row = board.length
-    //for col
-    for (let c = 0; c < col; c++) {
-        for(let r = 0; r < row - 2; r++) {
-            const num1 = Math.abs(board[r][c])
-            const num2 = Math.abs(board[r + 1][c])
-            const num3 = Math.abs(board[r + 2][c])
-            if (num1 === num2 && num1 === num3 && num1 !== 0) {
-                board[r][c] = -num1
-                board[r + 1][c] = -num2
-                board[r + 2][c] = -num3
-                isDone = false
-            }
-        }
-    }
     
-    //for rows
+    //check rows
     
-    for (let r = 0; r < row; r++) {
+    for (let r = 0 ; r < row; r++) {
         for(let c = 0; c < col - 2; c++) {
             const num1 = Math.abs(board[r][c])
             const num2 = Math.abs(board[r][c + 1])
             const num3 = Math.abs(board[r][c + 2])
-            if (num1 === num2 && num1 === num3 && num1 !== 0) {
+            if (num1 === num2 && num2 === num3 && num1 !== 0) {
                 board[r][c] = -num1
                 board[r][c + 1] = -num2
                 board[r][c + 2] = -num3
-                isDone = false
+                isDone = false    //since there are matching candies, so the board is not done
             }
         }
     }
-    
-    if (!isDone) {
-        for (let c = 0; c < col; c++) {
-            let pointer = row - 1
-            for(let r = row - 1; r >= 0; r--) {
+    //check cols
+    for(let c = 0 ; c < col; c++) {
+        for(let r= 0; r < row - 2; r++) {
+            const num1 = Math.abs(board[r][c])
+            const num2 = Math.abs(board[r + 1][c])
+            const num3 = Math.abs(board[r + 2][c])
+            if (num1 === num2 && num2 === num3 && num1 !== 0) {
+                board[r][c] = -num1
+                board[r + 1][c] = -num2
+                board[r + 2][c] = -num3
+                isDone = false    //since there are matching candies, so the board is not done
+            }
+        }
+    }
+
+    if (!isDone) {     
+        for(let c = 0 ; c < col; c++) {
+            let endIndex = row - 1
+            for(let r = row - 1; r >=0; r--) {
                 if (board[r][c] > 0) {
-                    board[pointer][c] = board[r][c]
-                    pointer--
+                    board[endIndex][c] = board[r][c]
+                    endIndex--
                 }
             }
-            for (let r = pointer; r >= 0; r--) {
+            for (let r = endIndex; r >= 0; r--) {
                 board[r][c] = 0
             }
         }
