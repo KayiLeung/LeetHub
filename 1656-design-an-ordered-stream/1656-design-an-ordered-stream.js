6b5bab9fdec1a0d94c72e33419c72e837b84ec1f
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-10-16 22:18:43 -0700

    Time: 325 ms (41.38%), Space: 54.5 MB (10.34%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.js
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.js
@@ -1,8 +1,13 @@
 /**
  * @param {number} n
  */
+
+//create an new array with n size and fill all values as null
+//assign an variable, pointer, to indicate ordered index
+//everytime when insert new values, check is there any ordered eles, if so, return ordered eles
+//else return emtpy array
 var OrderedStream = function(n) {
-    this.array = new Array(n).fill(null) // [null, null, null, null, null]
+    this.stream = new Array(n).fill(null)
     this.pointer = 0
 };
 
@@ -12,13 +17,13 @@ var OrderedStream = function(n) {
  * @return {string[]}
  */
 OrderedStream.prototype.insert = function(idKey, value) {
-    this.array[idKey - 1] = value
-    let outputted = []
-    while (this.array[this.pointer]) {
-        outputted.push(this.array[this.pointer])
+    this.stream[idKey - 1] = value
+    let res = []
+    while(this.stream[this.pointer]) {
+        res.push(this.stream[this.pointer])
         this.pointer++
     }
-    return outputted
+    return res
 };
 
 /** 
