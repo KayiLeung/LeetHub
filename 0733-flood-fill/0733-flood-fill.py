e4003900c3a6e1a18795fe86cd9cb1bd480150aa
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-10-25 14:24:37 -0700

    Time: 110 ms (56.43%), Space: 13.2 MB (98.22%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,24 +1,19 @@
-class Solution(object):
+def ffill(old_color, new_color, i, j, image, m, n):
+    
+    if i < m and i >= 0 and j < n and j >= 0 and image[i][j] == old_color:
+
+        image[i][j] = new_color
+        ffill(old_color, new_color, i, j+1, image, m, n)
+        ffill(old_color, new_color, i+1, j, image, m, n)
+        ffill(old_color, new_color, i-1, j, image, m, n)
+        ffill(old_color, new_color, i, j-1, image, m, n)
+
+    return image
+
+class Solution:
     def floodFill(self, image, sr, sc, color):
-        """
-        :type image: List[List[int]]
-        :type sr: int
-        :type sc: int
-        :type color: int
-        :rtype: List[List[int]]
-        """
-        m, n = len(image), len(image[0])
-        old_color = image[sr][sc]
         
-        if old_color == color:
+        if image[sr][sc] == color:
             return image
-        def dfs(r, c):
-            if image[r][c] == old_color:
-                image[r][c] = color
-                if r >= 1: dfs( r - 1, c)
-                if r + 1 < m: dfs(r + 1, c)
-                if c >= 1 : dfs (r, c - 1)
-                if c + 1 < n: dfs(r, c + 1)
-                    
-        dfs(sr, sc)
-        return image
+        m , n = len(image), len(image[0])
+        return ffill(image[sr][sc], color, sr, sc, image, m, n)
