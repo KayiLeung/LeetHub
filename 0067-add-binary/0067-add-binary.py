76709fe38616a4b1f867fce69600be989062ff20
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-10-26 02:57:31 -0700

    Time: 73 ms (24.49%), Space: 13.9 MB (25.35%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -4,26 +4,23 @@ class Solution:
         a, b = a.zfill(n), b.zfill(n)
         
         carry = 0
-        answer = []
+        answer = ''
         for i in range(n - 1, -1, -1):
             if a[i] == '1':
                 carry += 1
             if b[i] == '1':
                 carry += 1
-                
-            if carry % 2 == 1:
-                answer.append('1')
-            else:
-                answer.append('0')
             
-            carry //= 2
+            if carry % 2 == 0:
+                answer = '0' + answer
+            else:
+                answer = '1' + answer
+            
+            carry = carry // 2
         
         if carry == 1:
-            answer.append('1')
-        answer.reverse()
-        
-        return ''.join(answer)
-            
+            answer = '1' + answer
+        return answer
             
             
         
