f63f9f01ce6a7f4c5f16e976f1c050ca4bbf6db3
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-10-27 01:51:23 -0700

    Time: 195 ms (44.05%), Space: 50.7 MB (5.95%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.js
+++ 1314-matrix-block-sum/1314-matrix-block-sum.js
@@ -4,54 +4,43 @@
  * @return {number[][]}
  */
 var matrixBlockSum = function(mat, k) {
-    let sum = 0;
-    let dp = Array(mat.length + 1);
-    dp[0] = Array(mat[0].length).fill(0);
+    let m = mat.length
+    let n = mat[0].length
+    dp = Array.from(new Array(m), () => new Array(n).fill(0))
     
-    // sum of row el
-    for (let i = 0; i < mat.length; i++){
-        dp[i + 1] = Array(mat[0].length).fill(0);
-        for (let j = 0; j < mat[0].length; j++){
-            dp[i + 1][j] += mat[i][j] + sum;
-            sum = dp[i + 1][j];
+    for (let i = 0; i < m ; i++) {
+        let sum = 0
+        for(let j = 0; j < n; j++) {
+            dp[i][j] += mat[i][j] + sum
+            sum = dp[i][j]
         }
-        sum = 0;
-    }
-    
-    // sum of col el
-    for (let j = 0; j < mat[0].length; j++){
-        for (let i = 0; i < mat.length; i++){
-            dp[i + 1][j] += sum;
-            sum = dp[i + 1][j];
+    } 
+    for (let i = 1; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            dp[i][j] += dp[i - 1][j]
         }
-        sum = 0;
     }
-    
-    dp = dp.slice(1);
-    
-    // cal sum of blocks
-    for (let i = 0; i < mat.length; i++){
-        let r1 = Math.max(0, i - k);
-        let r2 = Math.min(mat.length - 1, i + k);
-        for (let j = 0; j < mat[0].length; j++){
-            let c1 = Math.max(0, j - k);
-            let c2 = Math.min(mat[0].length - 1, j + k);
+    console.log(dp)
+    for (let i = 0; i < m; i++) {
+        let r1 = Math.max(0, i - k)
+        let r2 = Math.min(m - 1, i + k)
+        for (let j = 0; j < n; j++) {
+            let c1 = Math.max(0, j - k)
+            let c2 = Math.min(n - 1, j + k)
             
-            let value = dp[r2][c2];
+            let value = dp[r2][c2]
             
-            if (r1 - 1 >= 0){
-                value -= dp[r1 - 1][c2];
+            if (r1 - 1 >= 0) {
+                value -= dp[r1 -1][c2]
             }
-            if (c1 - 1 >= 0){
+            if (c1 - 1 >= 0) {
                 value -= dp[r2][c1 - 1]
             }
-            if (r1 - 1 >= 0 && c1 - 1 >= 0){
-                value += dp[r1 - 1][c1 - 1];
+            if (r1 - 1 >= 0 && c1 - 1 >= 0) {
+                value += dp[r1 - 1][c1 - 1]
             }
-            mat[i][j] = value;
+            mat[i][j] = value
         }
     }
-    
-    return mat;
-    
+    return mat
 };
