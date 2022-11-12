d42b1e236e2949ebfbd3cec8f840cd3d610d1e4b
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-12 15:45:53 -0800

    Time: 109 ms (71.45%), Space: 45.1 MB (45.06%) - LeetHub

---
+++ 0046-permutations/0046-permutations.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    if(nums.length === 1){
+        return [nums];
+    }
+    
+    const permutations = [];
+        
+    for(let i=0; i<nums.length; i++){
+        const num = nums[i];
+        const nextPermutations = permute(nums.filter(n => n !== num));
+        
+        for(let j=0; j<nextPermutations.length; j++){
+            nextPermutations[j].unshift(num);
+        }
+        permutations.push(...nextPermutations);
+    }
+    
+    return permutations;
+};
