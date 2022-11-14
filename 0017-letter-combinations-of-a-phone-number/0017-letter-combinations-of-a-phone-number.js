27f521edb22dd8340bac785b14dff42cbfc37e5b
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-14 10:12:46 -0800

    Time: 104 ms (25.49%), Space: 42 MB (64.03%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+
+    
+const letters = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz'
+}
+
+var letterCombinations = function(digits) {
+    
+    if (!digits.length) return []
+    let res = []
+    const digit = digits[0]
+    let sub = letterCombinations(digits.slice(1))
+
+    
+    for(let l of letters[digit]) {
+        for (let ch of sub) {
+            res.push(l + ch)
+        }
+    }
+
+    if (res.length === 0) res = [...letters[digit]]
+    return res
+};
