36e57a6899422c1717f9f50d7d0d663468078487
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-15 10:56:34 -0800

    Time: 373 ms (97.11%), Space: 42.1 MB (94.73%) - LeetHub

---
+++ 0079-word-search/0079-word-search.js
@@ -0,0 +1,33 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    
+    for (let i = 0; i < board.length; i++) {
+        for(let j = 0; j < board[0].length; j++) {
+            if (explore(board,i, j, word, 0) === true) return true
+        }
+    }
+    
+    return false
+};
+    
+const explore = (board, row, col, word, i) => {
+    if (i === word.length) return true
+    
+    const rowInbound = 0 <=  row && row < board.length
+    const colInbound = 0 <=  col && col < board[0].length
+    if (!rowInbound || !colInbound) return false
+    const char = board[row][col]    
+    if (char !== word[i]) return false
+    board[row][col] = '*'
+    const result = explore(board, row - 1, col, word, i + 1) ||
+        explore(board, row + 1, col, word, i + 1) ||
+        explore(board, row, col - 1, word, i + 1) ||
+        explore(board, row, col + 1, word, i + 1)
+    
+    board[row][col] = char
+    return result
+}
