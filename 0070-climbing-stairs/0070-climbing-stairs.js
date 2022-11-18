fc814532c6c37c88300921e2b99ce4b8788cc8a5
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-17 17:36:23 -0800

    Time: 89 ms (59.64%), Space: 41.7 MB (77.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -4,7 +4,7 @@
  */
 var climbStairs = function(n, memo = {}) {
     if (n in memo) return memo[n]
-    if (n <= 2) return n
+    if (n <= 3) return n
     
     memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo)
     
