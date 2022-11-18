dc6da48075d2a06fc79bec399894b3c2c2f145b5
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-17 17:41:04 -0800

    Time: 100 ms (70.80%), Space: 44.7 MB (21.42%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    let chars = {}
+    
+    for (let ch of s) {
+        if(!(ch in chars)) {
+            chars[ch] = 0
+        }
+        chars[ch] += 1
+    }
+    let res = 0
+    let odd = false
+    for (let char in chars) {
+        const count = chars[char]
+        if (count % 2 === 1) odd = true
+        res += (Math.floor(count / 2) * 2)
+    }
+    return odd === true ? res + 1: res
+};
