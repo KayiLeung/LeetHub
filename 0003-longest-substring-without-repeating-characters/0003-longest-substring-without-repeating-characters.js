29c2c8c939d5b5a7e15fe198a36005c731834d23
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-18 18:38:05 -0800

    Time: 144 ms (66.19%), Space: 44.4 MB (97.41%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const chars = new Map()
+    let max = 0
+    let start = 0
+    for (let i = 0; i < s.length; i++) {
+        const ch = s[i]
+        if (chars.get(ch) >= start) start = chars.get(ch) + 1
+        chars.set(ch, i)
+        max = Math.max(max, i - start + 1)
+    }
+    return max
+};
