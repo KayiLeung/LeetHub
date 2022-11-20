22368f46a6ddfea8dcd7bf539811c042ff07b662
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-19 19:22:01 -0800

    Time: 279 ms (30.84%), Space: 50.8 MB (14.59%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -3,25 +3,25 @@
  * @param {number} amount
  * @return {number}
  */
+
+/*
+    
+*/
 var coinChange = function(coins, amount) {
     let res = _coinChange(coins, amount)
     return res === Infinity ? -1 : res
+    
 };
 
 const _coinChange = (coins, amount, memo = {}) => {
     if (amount === 0) return 0
     if (amount < 0) return Infinity
-    
     if (amount in memo) return memo[amount]
     let min = Infinity
-    for (const coin of coins) {
-        curr = 1 + _coinChange(coins, amount - coin, memo)
-        min = Math.min(curr, min)
+    for (let coin of coins) {
+        let curr = 1 + _coinChange(coins, amount - coin, memo)
+        min = Math.min(min, curr)
     }
-    
-    
-    
     memo[amount] = min
-    
     return memo[amount]
 }
