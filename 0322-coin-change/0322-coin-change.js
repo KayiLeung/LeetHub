6721147ac08773cb1d082f101be595274e366461
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-19 19:28:45 -0800

    Time: 180 ms (53.37%), Space: 45.7 MB (89.64%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -5,23 +5,24 @@
  */
 
 /*
-    
+    dp, create an array and equal to amount + 1
+    dp[0] = 0 means at amount 0, 0 coin needed
+    literate throught the amount and literate all possible coin in coins
+    if coin is greater than i continue
+    if not, look for 
 */
 var coinChange = function(coins, amount) {
-    let res = _coinChange(coins, amount)
-    return res === Infinity ? -1 : res
+    let dp = new Array(amount + 1).fill(Infinity)
+    dp[0] = 0
     
+    for (let i = 1; i <= amount; i++) {
+        let min = Infinity
+        for (let coin of coins) {
+            if (coin > i) continue
+            min = Math.min(min, dp[i - coin])
+        }
+        dp[i] = min + 1
+    }
+    return dp[amount] === Infinity ? -1 : dp[amount]
 };
 
-const _coinChange = (coins, amount, memo = {}) => {
-    if (amount === 0) return 0
-    if (amount < 0) return Infinity
-    if (amount in memo) return memo[amount]
-    let min = Infinity
-    for (let coin of coins) {
-        let curr = 1 + _coinChange(coins, amount - coin, memo)
-        min = Math.min(min, curr)
-    }
-    memo[amount] = min
-    return memo[amount]
-}
