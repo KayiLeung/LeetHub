b3cf616f4c7c5fc743c07a73b294a42c7069636c
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-20 01:27:56 -0800

    Time: 127 ms (71.40%), Space: 55.4 MB (18.59%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,34 +3,34 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    let visited = new Set()
     let count = 0
+    const visited = new Set()
     
-    for (let i = 0; i < grid.length; i++) {
-        for(let j = 0; j < grid[0].length; j++) {
-            if (explore(i, j, grid, visited)) count++
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[0].length; c++) {
+            if (grid[r][c] !== '0') {
+                if (travsal(r, c, grid, visited)) count++
+            }
         }
     }
     return count
 };
+const inbound = (row, col, m, n) => {
+    return row >= 0 && col >= 0 && row < m && col < n
+}
 
-
-
-const explore = (r, c, grid, visited) => {
-    const rowInbound = r >= 0 && r < grid.length
-    const colInbound = c >= 0 && c < grid[0].length
-    
-    if (!rowInbound || !colInbound) return false
+const travsal = (r, c, grid, visited) => {
+    if (!inbound(r, c, grid.length, grid[0].length)) return false
+    if (grid[r][c] !== '1') return false
     const pos = r + ',' + c
     if (visited.has(pos)) return false
     visited.add(pos)
     
-    if (grid[r][c] === '0') return false
     
-    explore(r + 1, c, grid, visited)
-    explore(r - 1, c, grid, visited)
-    explore(r, c + 1, grid, visited)
-    explore(r, c - 1, grid, visited)
+    travsal(r + 1, c, grid, visited)
+    travsal(r - 1, c, grid, visited)
+    travsal(r, c + 1, grid, visited)
+    travsal(r, c - 1, grid, visited)
     
     return true
 }
