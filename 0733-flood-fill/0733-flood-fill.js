20e3ae64842734d05da4f179c2676b7650a859d3
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-26 22:12:11 -0800

    Time: 136 ms (25.18%), Space: 44.6 MB (28.48%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -5,37 +5,28 @@
  * @param {number} color
  * @return {number[][]}
  */
-
-/*
-    bfs
-    find the ele from the given pos, use bfs and look for the same ele from four direction, it the neighbors are same as old color, change to new color, if not, pass it
-    edge case:
-        if not of bound, stop search
-        if the tile is not same as old color, stop the direction
-        return while no more search
-*/
 var floodFill = function(image, sr, sc, color) {
+    let visited = new Set()
     const oldColor = image[sr][sc]
-    const er = image.length
-    const ec = image[0].length
     
-    bfs(image, sr, sc, color, oldColor, er, ec)
-    
-
+    fillColor(image, sr,sc, color, oldColor, visited)
     
     return image
 };
 
-const inBound = (r, c, m , n) => {
-    return r >= 0 && c >= 0 && r < m && c < n
+const inbound = (r, c, er, ec) => {
+    return r >= 0 && c >= 0 && er > r && ec > c
 }
 
-const bfs = (image, sr, sc, color, oldColor, er, ec) => {
-    if (!inBound(sr, sc, er, ec) || image[sr][sc] !== oldColor || image[sr][sc] === color) return 
-
+const fillColor = (image, sr, sc, color, old, visited) => {
+    const pos = sr + ',' + sc
+    if (!inbound(sr, sc, image.length, image[0].length)) return 
+    if (pos in visited) return 
+    visited.add(pos)
+    if (image[sr][sc] !== old || image[sr][sc] === color) return
     image[sr][sc] = color
-    bfs(image, sr + 1, sc, color, oldColor, er, ec)
-    bfs(image, sr - 1, sc, color, oldColor, er, ec)
-    bfs(image, sr, sc + 1, color, oldColor, er, ec)
-    bfs(image, sr, sc - 1, color, oldColor, er, ec)
+    fillColor(image, sr - 1, sc, color, old, visited)
+    fillColor(image, sr + 1, sc, color, old, visited)
+    fillColor(image, sr, sc - 1, color, old, visited)
+    fillColor(image, sr, sc + 1, color, old, visited)
 }
