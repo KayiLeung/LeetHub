c9b4c0650573326addfb662d4ae34223bdc6166b
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-26 21:29:54 -0800

    Time: 71 ms (99.21%), Space: 43.8 MB (66.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,30 +3,21 @@
  * @param {string} t
  * @return {boolean}
  */
-
-/*
-    use hashmap, use char as key and count how many of that char appear in s
-    literate t, if char in hashmap, it value minus 1 
-    if char is not in hashmap return false
-    literate hashmap and see is it all key's value equal to 0
-    edge case:
-    if s and t having different length, return false
+/* frequency counter
+    use hashmap to count each chars freq
     
 */
 var isAnagram = function(s, t) {
     if (s.length !== t.length) return false
     let chars = {}
-    
     for (let char of s) {
-        if (!(char in chars)) chars[char] = 0
+        if (!chars.hasOwnProperty(char)) chars[char] = 0
         chars[char] += 1
     }
-    
     for (let char of t) {
-        if (!(char in chars)) return false
+        if (!chars.hasOwnProperty(char)) return false
         chars[char] -= 1
         if (chars[char] < 0) return false
     }
-    
     return true
 };
