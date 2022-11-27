8fb62d53a2414b3ad0917279ffb7115dfcfd5c75
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-27 00:19:16 -0800

    Time: 67 ms (85.71%), Space: 42.3 MB (20.24%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
@@ -1,7 +1,7 @@
 
 var MyQueue = function() {
-    this.q1 = []
-    this.q2 = []
+    this.stack1 = []
+    this.stack2 = []
 };
 
 /** 
@@ -9,38 +9,38 @@ var MyQueue = function() {
  * @return {void}
  */
 MyQueue.prototype.push = function(x) {
-    this.q1.push(x)
+    this.stack1.push(x)
 };
 
 /**
  * @return {number}
  */
 MyQueue.prototype.pop = function() {
-    if (this.q2.length === 0) {
-        while (this.q1.length) {
-            this.q2.push(this.q1.pop())
+    if (this.stack2.length === 0) {
+        while (this.stack1.length > 0) {
+            this.stack2.push(this.stack1.pop())
         }
     }
-    return this.q2.pop()
+    return this.stack2.pop()
 };
 
 /**
  * @return {number}
  */
 MyQueue.prototype.peek = function() {
-    if (this.q2.length === 0) {
-        while (this.q1.length) {
-            this.q2.push(this.q1.pop())
+    if (this.stack2.length === 0) {
+        while (this.stack1.length > 0) {
+            this.stack2.push(this.stack1.pop())
         }
     }
-    return this.q2.at(-1)
+    return this.stack2.at(-1)
 };
 
 /**
  * @return {boolean}
  */
 MyQueue.prototype.empty = function() {
-    return this.q1.length === 0 && this.q2.length === 0
+    return !(this.stack1.length > 0 || this.stack2.length > 0)
 };
 
 /** 
