ccada702aee62fcc11d1d2a7e0f25db0e8c93a10
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-26 21:20:22 -0800

    Time: 103 ms (81.80%), Space: 52.3 MB (7.66%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,12 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let min = prices[0]
-    let max = 0
+    let min = Infinity
+    let max = -Infinity
     
     for (let p of prices) {
-        max = Math.max(max, p - min)
         min = Math.min(min, p)
+        max = Math.max(max, p - min)
+        
     }
     return max
 };
