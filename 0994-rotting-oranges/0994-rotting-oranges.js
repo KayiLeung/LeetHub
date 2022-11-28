17c09ad70e3e11797af488780c06c3645ef7292f
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-28 15:47:58 -0800

    Time: 76 ms (94.71%), Space: 44.9 MB (55.35%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.js
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -5,47 +5,51 @@
 var orangesRotting = function(grid) {
     let rotten = []
     let fresh = 0
-    let res = 0
-    const check = new Set()
-    const er = grid.length
-    const ec = grid[0].length
+    let min = 0
+    let visited = new Set()
+    const m = grid.length
+    const n = grid[0].length
+    const deltas = [[1, 0], [0, 1],[-1, 0],[0, -1]]
     
-    const deltas = [[1,0],[0,1],[0,-1],[-1,0]]
-    const inbound = (r, c, m, n) => {
-        return r >= 0 && c >= 0 && r < m && c < n
-    }
-    for (let r = 0; r < er; r++) {
-        for (let c = 0; c < ec; c++) {
+    
+    
+    for (let r = 0; r < m; r++) {
+        for (let c = 0; c < n; c++) {
             if (grid[r][c] === 2) {
                 rotten.push([r, c])
+                visited.add(r + ',' + c)
             }
             if (grid[r][c] === 1) fresh++
         }
     }
+    
     if (fresh === 0) return 0
     while (rotten.length > 0 && fresh > 0) {
-        
         let len = rotten.length
         while (len > 0) {
             const [r, c] = rotten.shift()
             len--
             for (const [dr, dc] of deltas) {
-                const newRow = dr + r
-                const newCol = dc + c
-                if (inbound(newRow, newCol, er, ec)) {
-                    const pos = newRow + ',' + newCol
-                    if (check.has(pos)) continue
-                    if (grid[newRow][newCol] === 1) {
-                        rotten.push([newRow, newCol])
-                        grid[newRow][newCol] = 2
+                const newR = r + dr
+                const newC = c + dc
+                if (inbound(newR, newC, m, n)) {
+                    const pos = newR + ',' + newC
+                    if (visited.has(pos)) continue
+                    if (grid[newR][newC] === 1) {
+                        rotten.push([newR, newC])
+                        grid[newR][newC] = 2
                         fresh--
                     }
-                    check.add(pos)
+                    visited.add(pos)
                 }
             }
-            
         }
-        res++
+        min++
     }
-    return fresh === 0 ? res : -1
+
+    return fresh > 0 ? -1 : min
 };
+
+const inbound = (r, c, er, ec) => {
+    return r >= 0 && c >= 0 && r < er && c < ec
+}
