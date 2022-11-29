3b631623828568209e6f1f6322d07422bce9e880
KayiLeung <91761215+KayiLeung@users.noreply.github.com>
2022-11-28 23:20:39 -0800

    Time: 730 ms (60.67%), Space: 97.6 MB (77.43%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.js
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.js
@@ -1,6 +1,6 @@
 
 var TimeMap = function() {
-    this.keyTimeMap = new Map()
+    this.hashmap = {}
 };
 
 /** 
@@ -9,9 +9,9 @@ var TimeMap = function() {
  * @param {number} timestamp
  * @return {void}
  */
-TimeMap.prototype.set = function(key, value, timestamp, map = this.keyTimeMap) {
-    if (map.get(key) === undefined) map.set(key, [])
-    map.get(key).push([value, timestamp])
+TimeMap.prototype.set = function(key, value, timestamp) {
+    if (!this.hashmap.hasOwnProperty(key)) this.hashmap[key] = []
+    this.hashmap[key].push([value, timestamp])
 };
 
 /** 
@@ -19,22 +19,21 @@ TimeMap.prototype.set = function(key, value, timestamp, map = this.keyTimeMap) {
  * @param {number} timestamp
  * @return {string}
  */
-TimeMap.prototype.get = function(key, timestamp, map = this.keyTimeMap) {
-    if (map.get(key) === undefined) return ''
-    const arr = map.get(key)
-    let min = 0, max = arr.length - 1
-    let res = ''
-    
+TimeMap.prototype.get = function(key, timestamp) {
+    const arr = this.hashmap[key] || []
+    let min = 0
+    let max = arr.length - 1
+    let res = ""
     while (min <= max) {
-        const mid = ~~((min + max) /2)
-        const [value, time] = arr[mid]
-        
-        if (timestamp === time) return value
-        if (timestamp >= time) {
-            min = mid + 1
-            res = value
-        } else {
+        const mid = ~~((min + max) / 2)
+        const [val, time] = arr[mid]
+        if (time === timestamp) {
+            return val
+        } else if (time > timestamp) {
             max = mid - 1
+        } else {
+            min = mid + 1
+            res = val
         }
     }
     return res
